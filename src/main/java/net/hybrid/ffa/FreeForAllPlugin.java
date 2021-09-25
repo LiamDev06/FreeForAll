0e4c3e3b36fb226fbba5bc3150bd458796e46130
liam <71061493+LiamHBest0608@users.noreply.github.com>
2021-09-26 01:37:15 +0200

    First

---
+++ src/main/java/net/hybrid/ffa/FreeForAllPlugin.java
@@ -0,0 +1,17 @@
+package net.hybrid.ffa;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FreeForAllPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
